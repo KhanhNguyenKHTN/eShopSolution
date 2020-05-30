a903227d8949b0dea829e5f26f526fbd4c3ca5eb
nvkhanh123 <nguyenvankhanhxm123@gmail.com>
2020-05-30 11:59:18 +0700

    creata Database

---
+++ eShop.Data/EF/EShopDbContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using eShop.Data.EF;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace eShop.Data.EF
+{
+    public class EShopDbContextFactory : IDesignTimeDbContextFactory<EShopDBContext>
+    {
+        public EShopDBContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connectionString = configuration.GetConnectionString("eShopSolutionDb");
+
+            var optionsBuilder = new DbContextOptionsBuilder<EShopDBContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new EShopDBContext(optionsBuilder.Options);
+        }
+    }
+}
